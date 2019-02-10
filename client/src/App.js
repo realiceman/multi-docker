64a53cb8132e81864461967beaffb006a4aa9e4b
Youssef Harkati <youss.hark@gmail.com>
2019-02-09 20:08:25 -0500

    change name link fib

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Fibonacci Calculator</h1>
-            <Link to="/">Home</Link>
+            <Link to="/">Fib</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
